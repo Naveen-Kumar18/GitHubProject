4e097d91cb12495e623b47285c3b25589074c082
Naveen <naveenkumarmerigala@gmail.com>
2025-02-23 16:21:53 +0530

    change .

--- GitHubProject/src/com/Test.java
+++ GitHubProject/src/com/Test.java
@@ -3,7 +3,7 @@ package com;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("hello..");
-		System.out,println("hii");
+		System.out.println("hii");
 	}
 	
 	public static void sayHello() {
